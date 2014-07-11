52a3da38fd1564dafa101968762fff3850cb1951
Thinking Song <123wuming_123@163.com>
2014-07-11 11:51:03 +0800

    yo angular 初始化项目

---
+++ app/scripts/app.js
@@ -0,0 +1,33 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name mealingApp
+ * @description
+ * # mealingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('mealingApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
